ed0fac5d72b0bde5296df9b275de682cdee270de
Crespo-Vasquez Edison-Enrique <edison.crespo.ext@inetum.com>
2024-12-13 10:57:52 +0100

    Delete configure Jwt in program

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("src/Infrastructure/config/appsettings.json", optional: false, reloadOnChange: true);
 
 
-var key = builder.Configuration.GetSection("Jwt:key").Value;
-
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -23,34 +21,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-// Add connection data base
-builder.Services.AddDbContext<AplicationDbContext>(options => 
-      options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:DefaultConnection").Value));
 
-// Configure Independcy Inyection
-builder.Services.AddScoped<AccountService>();
-builder.Services.AddScoped<TokenService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
-//Configure Jwt Authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-options.TokenValidationParameters = new TokenValidationParameters
-{
-    ValidateIssuer = true,
-    ValidateAudience = true,
-    ValidateLifetime = true,
-    ValidateIssuerSigningKey = true,
-    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-    ValidAudience = builder.Configuration["Jwt:Audience"],
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
 
 //Configure Cors
 builder.Services.AddCors(options =>
@@ -63,9 +35,6 @@ builder.Services.AddCors(options =>
         .AllowCredentials();
     });
 });
-
-//Configure Mapper
-builder.Services.AddAutoMapper(typeof(UserProfile));
 
 
 var app = builder.Build();
